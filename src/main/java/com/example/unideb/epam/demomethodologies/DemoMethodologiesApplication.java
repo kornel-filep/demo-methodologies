ede2eafe614da17c30933c66f70e082a7716bd01
kfilep <kornel.filep@logmein.com>
2021-10-06 19:12:30 +0200

    double the !

--- src/main/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplication.java
+++ src/main/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplication.java
@@ -7,12 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoMethodologiesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello changes!");
-		System.out.println("Hello changes2!");
-		System.out.println("Hello Kornel!");
-		System.out.println("Hello !!!!");
-		System.out.println("Hello Veronika!");
-		System.out.println("Heyyyyyyyyyyyyyyy!");
+		System.out.println("Hello changes!!");
+		System.out.println("Hello changes2!!");
+		System.out.println("Hello Kornel!!");
+		System.out.println("Hello !!!!!!!!");
+		System.out.println("Hello Veronika!!!!!");
+		System.out.println("Heyyyyyyyyyyyyyyy!!!!!");
 		SpringApplication.run(DemoMethodologiesApplication.class, args);
 	}
 
