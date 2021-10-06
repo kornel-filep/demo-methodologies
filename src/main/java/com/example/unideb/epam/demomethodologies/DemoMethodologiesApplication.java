303c98db2e82c9ed5eecfcc10dae56c5d816ac63
kfilep <kornel.filep@logmein.com>
2021-10-06 19:10:48 +0200

    adding a lot of changes

--- src/main/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplication.java
+++ src/main/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplication.java
@@ -8,6 +8,11 @@ public class DemoMethodologiesApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello changes!");
+		System.out.println("Hello changes!");
+		System.out.println("Hello changes!");
+		System.out.println("Hello changes!");
+		System.out.println("Hello changes!");
+		System.out.println("Hello changes!");
 		SpringApplication.run(DemoMethodologiesApplication.class, args);
 	}
 
