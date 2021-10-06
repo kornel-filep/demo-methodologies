8aa4a0ace23629263a6cccac5bb7ace7f1a8d128
kfilep <kornel.filep@logmein.com>
2021-10-06 19:06:25 +0200

    Initial commit

---
+++ src/main/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.unideb.epam.demomethodologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMethodologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMethodologiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unideb/epam/demomethodologies/DemoMethodologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unideb.epam.demomethodologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMethodologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
